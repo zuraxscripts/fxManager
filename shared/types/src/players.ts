b6314e42a65d697dd2e17aa947fd1e27442ab3c0
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-13 00:04:06 +0100

    chore(shared/types): updated player interfaces

--- shared/types/src/players.ts
+++ shared/types/src/players.ts
@@ -1,13 +1,21 @@
+export interface PlayerIdentifiers {
+  license: string;
+  fivem?: string;
+  discord?: string;
+  steam?: string;
+}
+
 export interface Player {
   id: number;
-  license: string;
   name: string;
+  playtime: number;
+  identifiers: PlayerIdentifiers;
   firstSeen: Date;
   lastSeen: Date;
 }
 
 export interface OnlinePlayer extends Player {
-  serverNetId: number;
-  ping: number;
-  identifiers: string[];
+  serverId: number;
+  ping?: number;
+  health: number;
 }
