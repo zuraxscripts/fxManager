20bf0924b708643ba5f941ac4f618c76116916e1
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-14 00:43:37 +0100

    refactor(core/common): completed ansi colour dict and moved to separate file

--- packages/core/src/common/utils.ts
+++ packages/core/src/common/utils.ts
@@ -1,36 +1,8 @@
 import { LogSegment } from '@fxmanager/types';
+import { colorIdToHex } from './data';
 
 export function getHexColourFromAnsi(index: number): string {
-  const palette: Record<number, string> = {
-    21: '#0000ff',
-    23: '#005f5f',
-    33: '#0087ff',
-    42: '#00d787',
-    46: '#00ff00',
-    51: '#00ffff',
-    66: '#5f8787',
-    73: '#5fafaf',
-    83: '#5af761',
-    135: '#af5fff',
-    161: '#d7005f',
-    165: '#d700ff',
-    177: '#d787ff',
-    196: '#ff0000',
-    208: '#ff8700',
-    220: '#ffd700',
-    226: '#ffff00',
-    201: '#ff00ff',
-    231: '#ffffff',
-    232: '#080808',
-    244: '#808080',
-    250: '#bcbcbc',
-  };
-
-  if (palette[index]) return palette[index];
-
-  DEV: console.warn(`[core - utils] No colour found for index: ${index}`);
-
-  return '#ffffff';
+  return colorIdToHex[index] ?? '#ffffff';
 }
 
 export function parseAnsiToSegments(ansiStr: string): LogSegment[] {
