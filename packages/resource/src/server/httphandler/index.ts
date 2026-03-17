fb4c25a6c397b0967f905798ca9d7cc643a6bce6
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-17 23:45:46 +0100

    feat(resource/server): instantiate httpserver and add drop endpoint

--- packages/resource/src/server/httphandler/index.ts
+++ packages/resource/src/server/httphandler/index.ts
@@ -1,9 +1,25 @@
-import { API_TOKEN } from "../utils/env";
-import { HttpServer } from "./class";
+import z from 'zod';
+import { API_TOKEN } from '../utils/env';
+import { HttpServer } from './class';
 
 const api = new HttpServer(API_TOKEN);
 
-// ToDo
-api.post('/drop', (req) => {
-  return { status: 200, headers: {} };
-})
+api.post(
+  '/drop',
+  {
+    schema: z.object({
+      serverId: z.number(),
+      reason: z.string(),
+    }),
+  },
+  ({ body }) => {
+    const { serverId, reason } = body;
+
+    DropPlayer(String(serverId), reason);
+
+    return {
+      status: 200,
+      body: { success: true, data: null },
+    };
+  },
+);
