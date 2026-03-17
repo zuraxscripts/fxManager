fcfb2358a46ecff6d0e1760083876e006bde45a4
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-18 00:38:53 +0100

    chore(panel-ui/components): removed commented code from AppLayout

--- packages/panel-ui/src/components/sidebar/index.tsx
+++ packages/panel-ui/src/components/sidebar/index.tsx
@@ -3,30 +3,10 @@ import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { Outlet } from 'react-router-dom';
 
 export default function AppLayout() {
-  // const location = useLocation();
-
-  // const getPageTitle = (pathname: string) => {
-  //   if (pathname === '/') return '';
-
-  //   const pathParts = pathname.split('/').filter(Boolean);
-  //   const lastPart = pathParts[pathParts.length - 1] || 'Dashboard';
-
-  //   return lastPart.charAt(0).toUpperCase() + lastPart.slice(1);
-  // };
-
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        {/* <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-8" />
-            <span className="text-sm font-semibold text-foreground">
-              {getPageTitle(location.pathname)}
-            </span>
-          </div>
-        </header> */}
         <div className="flex flex-1 flex-col gap-4 p-4">
           <Outlet />
         </div>
