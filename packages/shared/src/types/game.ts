de4736fda01dab19849907adc31035c107590f97
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-26 19:32:10 +0100

    feat(shared/types): game types -> (used for comms between game resource & fxManager)

---
+++ packages/shared/src/types/game.ts
@@ -0,0 +1,39 @@
+import type { Player } from './players';
+
+export type BanDataCard =
+	| {
+			reason: string;
+			permanent: false;
+			createdAt: Date;
+			expiresAt: Date;
+	  }
+	| {
+			reason: string;
+			permanent: true;
+			createdAt: Date;
+	  };
+
+export type DeferralCheckResponse =
+	| {
+			access: true;
+	  }
+	| {
+			access: false;
+			type: 'ban';
+			ban: BanDataCard;
+	  }
+	| {
+			access: false;
+			type: 'error';
+			reason: string;
+	  };
+
+export interface OnlinePlayer extends Player {
+	serverId: number;
+	ping?: number;
+	health: number;
+}
+
+export type GameEventPayload =
+	| { event: 'player.join'; data: OnlinePlayer }
+	| { event: 'player.drop'; data: { serverId: number } };
