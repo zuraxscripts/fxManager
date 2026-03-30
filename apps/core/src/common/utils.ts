11ad1881ad271549074e1450072b2437bd73a789
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-30 22:46:46 +0200

    feat(core/utils): added isFxManagerSetup helper

--- apps/core/src/common/utils.ts
+++ apps/core/src/common/utils.ts
@@ -1,1 +1,7 @@
+import { repo } from "@fxmanager/database";
+
 export const isProduction = process.env.NODE_ENV === 'production';
+
+export function isFxManagerSetup() {
+	return repo.auth.countUsers() > 0;
+}
