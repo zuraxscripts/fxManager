c4e08e8bd0f77e6ff14698e979ccde7f99b27e48
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-31 17:13:07 +0200

    feat(core/routes): added start/stop/restart methods to api routes

--- apps/core/src/routes/api/server.ts
+++ apps/core/src/routes/api/server.ts
@@ -1,11 +1,55 @@
 import type { FastifyPluginAsync } from "fastify"
 import type { RouteModule } from "../../types";
 import { sessionAuth } from "../../middleware/auth";
+import { PermissionManager } from "@fxmanager/shared/utils";
+import { UserPermissions } from "@fxmanager/shared/constants";
 
 const ServerEndpoints: RouteModule['handler'] = async (fastify, options) => {
-	const { pm, gm } = options;
+	const { pm } = options;
 	
   fastify.addHook('preHandler', sessionAuth);
+
+	fastify.post('/start', async (request, reply) => {
+		const { admin } = request;
+
+		const allowed = PermissionManager.has(admin!.permissions, UserPermissions.SERVER_ACTIONS);
+
+		if (!allowed) {
+      return reply.code(403).send({ error: 'Not authorized' }); 
+    }
+
+    const result = await pm.start();
+
+    return reply.code(result ? 200 : 500).send({ success: result });
+	});
+
+	fastify.post('/stop', async (request, reply) => {
+		const { admin } = request;
+
+		const allowed = PermissionManager.has(admin!.permissions, UserPermissions.SERVER_ACTIONS);
+
+		if (!allowed) {
+      return reply.code(403).send({ error: 'Not authorized' }); 
+    }
+
+    const result = await pm.stop();
+
+    return reply.code(result ? 200 : 500).send({ success: result });
+	});
+
+	fastify.post('/restart', async (request, reply) => {
+		const { admin } = request;
+
+		const allowed = PermissionManager.has(admin!.permissions, UserPermissions.SERVER_ACTIONS);
+
+		if (!allowed) {
+      return reply.code(403).send({ error: 'Not authorized' }); 
+    }
+
+    const result = await pm.restart();
+
+    return reply.code(result ? 200 : 500).send({ success: result });
+	});
 };
 
 export default {
