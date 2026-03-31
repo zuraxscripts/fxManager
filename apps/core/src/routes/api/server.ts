f2bb04c74289a69dd2d1d7cea1f1a13ace5966a2
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-31 16:47:31 +0200

    chore(core/routes): added auth middleware to api/server routes

--- apps/core/src/routes/api/server.ts
+++ apps/core/src/routes/api/server.ts
@@ -1,7 +1,10 @@
 import type { FastifyPluginAsync } from "fastify"
 import type { RouteModule } from "../../types";
+import { sessionAuth } from "../../middleware/auth";
 
-const ServerEndpoints: FastifyPluginAsync = async (fastify, options) => {};
+const ServerEndpoints: FastifyPluginAsync = async (fastify, options) => {
+  fastify.addHook('preHandler', sessionAuth);
+};
 
 export default {
 	prefix: '/server',
