dda377e8c1e8c02087e60fce199286bf8257e32e
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-16 11:34:37 +0200

    feat(core/modules): buffer manager

---
+++ apps/core/src/modules/buffer.manager.ts
@@ -0,0 +1,13 @@
+export class LogBuffer<T> {
+  private buffer: T[] = [];
+  constructor(private limit: number = 1000) {}
+
+  push(event: T) {
+    this.buffer.push(event);
+    if (this.buffer.length > this.limit) this.buffer.shift();
+  }
+
+  getHistory() {
+    return this.buffer;
+  }
+}
