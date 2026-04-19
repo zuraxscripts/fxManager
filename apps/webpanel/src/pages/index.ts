45c494ed95fb752d4bd67943517c87cc144989d0
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-19 13:59:35 +0200

    fix(webpanel/pages): added playerview to router

--- apps/webpanel/src/pages/index.ts
+++ apps/webpanel/src/pages/index.ts
@@ -5,6 +5,7 @@ import OnlinePlayerListPage from './dashboard/playerList';
 import ConsolePage from './console';
 import PlayersPage from './players';
 import SettingsPage from './settings';
+import PlayerView from './players/playerview';
 
 type RouteConfig = {
 	path: string;
@@ -18,7 +19,7 @@ export const routes: RouteConfig[] = [
 	{ path: '/dashboard', element: DashboardPage },
 	{ path: '/dashboard/players', element: OnlinePlayerListPage },
 	{ path: '/players', element: PlayersPage },
-	// { path: '/players/:playerId', element: PlayerView },
+	{ path: '/players/:playerId', element: PlayerView },
 	{ path: '/console', element: ConsolePage },
 	{ path: '/settings', element: SettingsPage },
 ];
