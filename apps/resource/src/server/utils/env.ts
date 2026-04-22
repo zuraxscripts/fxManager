8aacdec463ee547b65c1f9c3cfaa15badc30c28c
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-22 12:34:01 +0200

    fix(resource/server): env util module using wrong default api port & hostname

--- apps/resource/src/server/utils/env.ts
+++ apps/resource/src/server/utils/env.ts
@@ -1,6 +1,6 @@
 const API_TOKEN = GetConvar('resource-api-token', '');
-const PORT = GetConvarInt('api-port', 4005);
-const HOSTNAME = `localhost:${PORT}`;
+const PORT = GetConvarInt('api-port', 3000);
+const HOSTNAME = `127.0.0.1:${PORT}`;
 
 const uuidV4Regex =
 	/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
