57dd5357b6c7c86d90ac9f2c82e92cd6639e155e
Maximus7474 <the.maximus.7474@gmail.com>
2026-04-26 23:43:50 +0200

    feat(shared/utils): added get group data from PermissionManager

--- packages/shared/src/utils/permissions.ts
+++ packages/shared/src/utils/permissions.ts
@@ -1,7 +1,36 @@
-import type { UserPermissionsType } from '@fxmanager/shared/types';
+import type { AdminGroup, UserPermissionsType } from '@fxmanager/shared/types';
 import { UserPermissions } from '@fxmanager/shared/constants';
 
+// ToDo: move to dynamic loading from DB
+const GROUPS: AdminGroup[] = [
+	{
+		label: 'Master Account',
+		permissions: 1073741824,
+		colour: '#FF0000',
+		icon: 'Star',
+	},
+	{
+		label: 'Development',
+		permissions: 30720,
+		colour: '#00FF00',
+		icon: 'FileCode',
+	},
+	{
+		label: 'Management',
+		permissions: 36863,
+		colour: '#0000FF',
+		icon: 'UserRoundKey',
+	},
+	{
+		label: 'Moderation',
+		permissions: 1991,
+		colour: '#ff6600',
+		icon: 'Shield',
+	},
+];
+
 export class PermissionManager {
+	private static groups: AdminGroup[] = GROUPS;
 	constructor() {}
 
 	static has(userBitfield: number, required: UserPermissionsType): boolean {
@@ -27,5 +56,18 @@ export class PermissionManager {
 
 	static revoke(userBitfield: number, toRemove: UserPermissionsType): number {
 		return userBitfield & ~toRemove;
+	}
+
+	static loadGroups(groups: AdminGroup[]) {
+		this.groups = groups;
+	}
+
+	static getGroup(permission: number): AdminGroup | null {
+		const group =
+			GROUPS.find(
+				(group) => (permission & group.permissions) === group.permissions,
+			) ?? null;
+
+		return group;
 	}
 }
