5770976f8baa9aeffe016cc1a494d65eeb049169
Maximus7474 <the.maximus.7474@gmail.com>
2026-05-01 15:58:22 +0200

    feat(core/routes): added to internal group resource endpoints

--- apps/core/src/routes/internal/index.ts
+++ apps/core/src/routes/internal/index.ts
@@ -1,10 +1,16 @@
 import PlayerModule from './players';
+import ResourceModule from './resources';
 import type { RouteModule } from '../../types';
 
 const internalRoutes: RouteModule['handler'] = async (fastify, options) => {
 	fastify.register(PlayerModule.handler, {
 		...options,
 		prefix: PlayerModule.prefix,
+	});
+  
+	fastify.register(ResourceModule.handler, {
+		...options,
+		prefix: ResourceModule.prefix,
 	});
 };
 
---
+++ apps/core/src/routes/internal/resources.ts
@@ -0,0 +1,36 @@
+import type { ResourceData } from '@fxmanager/shared/types';
+import { resourceAuth } from '../../middleware/resource';
+import { resourceManager } from '../../modules/resource.manager';
+import type { RouteModule } from '../../types';
+
+const ResourceEndpoints: RouteModule['handler'] = async (fastify) => {
+
+	fastify.addHook('preHandler', resourceAuth);
+
+	fastify.post('/update', (request) => {
+		const { body } = request;
+
+		resourceManager.handleResourceUpdate({
+			event: 'update',
+			data: body as ResourceData,
+		});
+
+		return { ack: true };
+	});
+
+	fastify.post('/refresh', (request) => {
+		const { body } = request;
+
+		resourceManager.handleResourceUpdate({
+			event: 'refresh',
+			data: body as ResourceData[],
+		});
+
+		return { ack: true };
+	});
+};
+
+export default {
+	prefix: '/resources',
+	handler: ResourceEndpoints,
+} satisfies RouteModule;
