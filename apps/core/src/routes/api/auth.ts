f052bd085b9b930ef83cf1dcbc1d8be80105980f
Maximus7474 <the.maximus.7474@gmail.com>
2026-06-19 12:55:45 +0200

    chore(core/routes/api): removed setup from auth module

--- apps/core/src/routes/api/auth.ts
+++ apps/core/src/routes/api/auth.ts
@@ -1,62 +1,25 @@
 import { Type, type Static } from '@sinclair/typebox';
 import type { FastifyPluginAsync } from 'fastify';
 import { repo } from '@fxmanager/database';
-import { UserPermissions } from '@fxmanager/shared/constants';
 
-import {
-	COOKIE_NAME,
-	isFxManagerSetup,
-	isProduction,
-} from '../../common/utils';
+import { COOKIE_NAME, isProduction } from '../../common/utils';
 import type { RouteModule } from '../../types';
 
-const SetupBody = Type.Object({
+const LoginBody = Type.Object({
 	username: Type.String(),
 	password: Type.String(),
 });
 
-type SetupBodyType = Static<typeof SetupBody>;
+type LoginBodyType = Static<typeof LoginBody>;
 
 const AuthEndpoints: FastifyPluginAsync = async (fastify) => {
-	fastify.post<{ Body: SetupBodyType }>(
-		'/setup',
-		{ schema: { body: SetupBody } }, // validates + types the body
-		async (request, reply) => {
-			if (isFxManagerSetup()) {
-				return reply.code(403).send({ error: 'Already set up' });
-			}
-
-			const { username, password } = request.body;
-
-			const user = await repo.auth.createUser(
-				username,
-				password,
-				UserPermissions.MASTER,
-				true,
-			);
-
-			const session = repo.auth.createSession(user.id);
-
-			return reply
-				.setCookie(COOKIE_NAME, session.id, {
-					httpOnly: true, // not accessible from JS
-					secure: isProduction, // HTTPS only in prod
-					sameSite: 'lax',
-					path: '/',
-					maxAge: 60 * 60 * 24 * 7, // 1 week in seconds
-				})
-				.code(201)
-				.send({ success: true });
-		},
-	);
-
 	fastify.post(
 		'/login',
 		{
-			schema: { body: SetupBody },
+			schema: { body: LoginBody },
 		},
 		async (request, reply) => {
-			const { username, password } = request.body as SetupBodyType;
+			const { username, password } = request.body as LoginBodyType;
 
 			const user = await repo.auth.verifyPassword(username, password);
 			if (!user) {
